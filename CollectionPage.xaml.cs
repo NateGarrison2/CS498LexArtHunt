3d7527a048fe30942bc903debbd1effb0986c08c
gunnerkline <gunner.v.kline@gmail.com>
2026-04-20 12:47:05 -0400

    Added collection delete button that empties collection.

--- CollectionPage.xaml.cs
+++ CollectionPage.xaml.cs
@@ -19,7 +19,14 @@ namespace LexArtHunt
         protected override void OnAppearing()
         {
             base.OnAppearing();
-            
+
+            // Load the collection when the page appears
+            LoadCollectedArt();
+        }
+
+        // Load or refresh the collection view items
+        private void LoadCollectedArt()
+        {
             using var userDb = new UserDbContext();
             using var appDb = new AppDbContext();
 
@@ -47,6 +54,27 @@ namespace LexArtHunt
                 // Clear selection so the item can be tapped again on return
                 CollectionView.SelectedItem = null;
                 await Navigation.PushAsync(new DetailsPage(vm.ArtItem));
+            }
+        }
+
+        // Delete entire collection
+        private async void OnDeleteCollectionClicked(object sender, EventArgs e)
+        {
+            using var userDb = new UserDbContext();
+
+            // Warn the user before deleting and confirm the action
+            bool answer = await DisplayAlertAsync("Confirm", "Are you sure you want to delete your entire collection?", "Yes", "No");
+            if (answer)
+            {
+                if(!userDb.CollectedItems.Any())
+                {
+                    await DisplayAlertAsync("Notice!", "Your collection is already empty.", "OK");
+                    return;
+                }
+                userDb.CollectedItems.RemoveRange(userDb.CollectedItems);
+                await userDb.SaveChangesAsync();
+                LoadCollectedArt(); // Refresh the collection view
+                await DisplayAlertAsync("Success", "Successfully deleted entire collection.", "OK");
             }
         }
     }
